eb414bc1fc7fa71531cd357f8a58473c10486e83
vaibhav17223 <vaibhavvhanmane6@gmail.com>
2026-09-23 12:14:17 +0530

    Calculate area of a circle
    Fix variable name typo and correct Printf function.

---
+++ VKVC3.c
@@ -0,0 +1,13 @@
+/*
+  program (3) write the program to calculat the are of a circle.
+Solution (1) Predefined Values
+*/
+#include <stdio.h>
+int main ()
+{
+float rodius=1.1;
+float area;
+area = (3.14* radius *radius);
+Printf ("\n Area of circle = %f", area);
+return 0;
+}
