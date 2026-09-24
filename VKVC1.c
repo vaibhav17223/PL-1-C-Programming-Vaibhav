cd10219599def9df1da4f7b564f17d4d7aa4729d
vaibhav17223 <vaibhavvhanmane6@gmail.com>
2026-09-24 13:21:13 +0530

    Implement student information display program
    This program demonstrates basic Input/Output functions using different data types.

---
+++ VKVC1.c
@@ -0,0 +1,27 @@
+/*
+Program (1) Write the program to make use of
+basic Input/Output functions using different
+data types.
+
+Solution (1) -> Predefined values
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int rollnum;
+    float per;
+    char grade;
+
+    rollnum = 55;
+    per = 88.88;
+    grade = 'A';
+
+    printf("---Student Information---\n");
+    printf("\nRoll Number : %d", rollnum);
+    printf("\nPercentage : %.2f", per);
+    printf("\nGrade : %c", grade);
+
+    return 0;
+}
